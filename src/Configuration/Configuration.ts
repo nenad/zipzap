a8c37542df0f2e86c291cd3c7c19f07e0c1e1468
Nenad Stojanovikj <nekk1@live.com>
2018-04-29 21:57:58 +0200

    Add request enhancement

--- src/Configuration/Configuration.ts
+++ src/Configuration/Configuration.ts
@@ -1,6 +1,5 @@
-import { LoadURLOptions } from "electron";
+import { app, LoadURLOptions, session } from "electron";
 import { readFileSync } from "fs";
-import { format } from "path";
 import URLDashboard from "../Dashboard/URLDashboard";
 
 enum DashboardType {
@@ -9,30 +8,52 @@ enum DashboardType {
   Video = "video",
 }
 
-class ConfigItem {
-  public type!: DashboardType;
-  public uri!: string;
-  public duration!: number;
-  public options?: LoadURLOptions;
+interface IConfiguration {
+  dashboards: IDashboardConfig[];
+  requestEnhancements?: IRequestEnhancement[];
+}
+
+interface IDashboardConfig {
+  type: DashboardType;
+  uri: string;
+  duration: number;
+  options: LoadURLOptions;
+}
+
+interface IRequestEnhancement {
+  patterns: string[];
+  headers: Map<string, string>;
 }
 
 export default class Configuration {
-
   public static LoadFromFile(filePath: string): Configuration {
     // JSON Schema validation?
     const contents = readFileSync(filePath);
-    const config = JSON.parse(contents.toString());
+    const config: IConfiguration = JSON.parse(contents.toString());
     const dashboards: IDashboard[] = [];
 
-    config.dashboards.forEach((item: ConfigItem) => {
+    config.dashboards.forEach((item: IDashboardConfig) => {
       switch (item.type) {
-        case DashboardType.Page: {
+        case DashboardType.Page:
+        default:
           dashboards.push(new URLDashboard(item.uri, item.duration, item.options));
           break;
-        }
-        default:
-          throw new Error("Unrecognized type: " + item.type);
       }
+    });
+
+    app.once("ready", () => {
+      if (!config.requestEnhancements) { return; }
+
+      config.requestEnhancements.forEach((item: IRequestEnhancement) => {
+        session.defaultSession!.webRequest.onBeforeSendHeaders(
+          { urls: item.patterns },
+          (details: any, callback: (response: any) => void) => {
+            for (const [key, value] of Object.entries(item.headers)) {
+              details.requestHeaders[key] = value;
+              callback({ cancel: false, requestHeaders: details.requestHeaders });
+            }
+          });
+      });
     });
 
     return new Configuration(dashboards, false, false);
